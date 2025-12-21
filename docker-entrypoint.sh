e4e91ddd7706ab558d4b9bf605936000fad39542
Ivan I. Grushin <mcgru@womanup.online>
2025-12-22 00:15:30 +0700

    multiple models

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,17 +6,26 @@ sleep 2
 # -----------------------------------
 echo 'alias lms="~/.cache/lm-studio/bin/lms"' > ~/.bashrc
 
+cp -f /http-server-config.json /root/.cache/lm-studio/.internal/http-server-config.json
+
 /squashfs-root/lm-studio --no-sandbox &
 sleep 30
 ~/.cache/lm-studio/bin/lms server start --cors &
 
 
 sleep 5
-# ~/.cache/lm-studio/bin/lms get ${MODEL_PATH}
-~/.cache/lm-studio/bin/lms load --gpu 0.3 --ttl 3600 --context-length ${CONTEXT_LENGTH:-16384} ${MODEL_IDENTIFIER} &
+### ~/.cache/lm-studio/bin/lms get ${MODEL_PATH}
+##~/.cache/lm-studio/bin/lms load --gpu 0.3 --ttl 3600 --context-length ${CONTEXT_LENGTH:-16384} ${MODEL_IDENTIFIER} &
+#~/.cache/lm-studio/bin/lms load  --ttl 3600 --context-length ${CONTEXT_LENGTH:-16384} ${MODEL_IDENTIFIER} &
+for m in ${MODEL_IDENTIFIER} ; do
+##~/.cache/lm-studio/bin/lms load  --ttl 3600 --context-length ${CONTEXT_LENGTH:-16384} $m &
+~/.cache/lm-studio/bin/lms load  --ttl 60000 --context-length ${CONTEXT_LENGTH:-16384} -y $m
+done
 
 sleep 20
-cp -f /http-server-config.json /root/.cache/lm-studio/.internal/http-server-config.json
+
+### ???
 x11vnc -display :99 -forever -rfbauth /root/.vnc/passwd -quiet -listen 0.0.0.0 -xkb
 
+### ???
 /bin/bash
